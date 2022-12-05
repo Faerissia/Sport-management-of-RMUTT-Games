9f36c7d3c044d7358c5818ae713c872d5bf64ce8
FAERIS <firstlnw0099@gmail.com>
2022-12-05 18:33:09 +0700

    login&regis inprogress

--- index.js
+++ index.js
@@ -30,6 +30,13 @@ const ifNotLoggedIn = (req, res, next) =>{
     next();
 }
 
+const ifLoggedIn =(req, res, next)=>{
+    if (!req.session.isLoggedIn){
+        return res.rendirect('/dashboard');
+    }
+    next();
+}
+
 //root rage
 app.get('/', ifNotLoggedIn, (req, res, next)=>{
     dbConnection.execute("SELECT email FROM account WHERE id =?",[req.session.userID])
@@ -39,5 +46,23 @@ app.get('/', ifNotLoggedIn, (req, res, next)=>{
         })
     })
 })
+
+//register page
+app.post('/register', ifLoggedIn,[
+    body('user_email', 'Invalid Email Address!').isEmail().custom((value) =>{
+        return dbConnection.execute('SELECT email FROM users WHERE email = ?',[value])
+        .then(([rows]) => {
+            if(rows.length>0){
+                return Promise.reject('This email already in use!');
+            }
+            return true;  
+        })
+    }),
+    body('user_name', 'Username os empty!').trim().not().isEmpty(),
+    body('User_pass', 'The password must be of minimun length 6 characters').trim().isLength({ min:6}),
+],//end of post data validation
+
+)
+
 
 app.listen(3000, () => console.log("Server is running..."))
