ef06d72b363cfb50285416fe7f67ba7521136613
FAERIS <firstlnw0099@gmail.com>
2022-12-05 16:30:35 +0700

    update delete old file
    add Login page and create Account inprogress

--- views/database.js
+++
@@ -1,9 +0,0 @@
-const mysql = require("mysql");
-const dbConnection = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "thesis"
-}).promise()
-
-module.export = dbConnection;
