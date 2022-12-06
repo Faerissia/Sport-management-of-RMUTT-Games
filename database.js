f79afe89706ab0421287a51e8106d453f357b085
Faerissia <68456363+Faerissia@users.noreply.github.com>
2022-12-06 15:36:24 +0700

    re system login & regis

--- database.js
+++ database.js
@@ -1,9 +1,8 @@
-const mysql = require("mysql2");
+const mysql = require('mysql2');
 const dbConnection = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "thesis"
-}).promise()
-
+    host     : 'localhost', // MYSQL HOST NAME
+    user     : 'root', // MYSQL USERNAME
+    password : '', // MYSQL PASSWORD
+    database : 'thesis' // MYSQL DB NAME
+}).promise();
 module.exports = dbConnection;
