c0f960f2a5c14f3588589f6835e68e6c95143134
FAERIS <firstlnw0099@gmail.com>
2022-12-07 15:37:08 +0700

    update login&register
    Login Register successfully
    Role user inprogress

--- database.js
+++
@@ -1,8 +0,0 @@
-const mysql = require('mysql2');
-const dbConnection = mysql.createPool({
-    host     : 'localhost', // MYSQL HOST NAME
-    user     : 'root', // MYSQL USERNAME
-    password : '', // MYSQL PASSWORD
-    database : 'thesis' // MYSQL DB NAME
-}).promise();
-module.exports = dbConnection;
---
+++ utils/dbConnection.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+
+const dbConnection = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'thesis'
+});
+
+module.exports = dbConnection.promise();
