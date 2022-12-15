04dc51c02b65808b2701115c5abdba615f94b126
wasawin <wasawin.14@gmail.com>
2022-12-15 23:14:58 +0700

    up 15

--- app.js
+++ app.js
@@ -1,41 +1,56 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const routes = require('./routes');
+const http = require('http');
+const path = require('path');
+const session = require('express-session');
+const app = express();
+const mysql = require('mysql');
+let bodyParser=require("body-parser");
+let flash = require('express-flash');
+const dbConnection = require('./util/db');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
-var app = express();
+dbConnection.connect();
+global.db = dbConnection;
+ 
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//routes variable
+const user = require('./routes/user');
+const uni = require('./routes/uni');
+const faculty = require('./routes/faculty');
+const sport = require('./routes/sport');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// all environments
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('public'))
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+              secret: 'secret',
+              resave: 'true',
+              saveUninitialized: true,
+              cookie: { maxAge: 60000 }
+            }))
+app.use(flash());
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+//routes
+app.use('/uni', uni);
+app.use('/faculty',faculty);
+app.use('/sport',sport);
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+// development only
+app.get('/', routes.index);//call for main index page
+app.get('/signup', user.signup);//call for signup page
+app.post('/signup', user.signup);//call for signup post 
+app.get('/login', routes.index);//call for login page
+app.post('/login', user.login);//call for login post
+app.get('/home/dashboard', user.dashboard);//call for dashboard page after login
+app.get('/home/logout', user.logout);//call for logout
+app.get('/home/profile',user.profile);//to render users profile
 
-module.exports = app;
+//Middleware
+app.listen(3000)
---
+++ routes/sport.js
@@ -0,0 +1,148 @@
+let express = require('express');
+let router = express.Router();
+let dbConnection = require('../util/db');
+
+// display sport page
+router.get('/', (req, res, next) => {
+    dbConnection.query('SELECT * FROM sport ORDER BY sportID asc', (err, rows) => {
+        if (err) {
+            req.flash('error', err);
+            res.render('sport', { data: '' });
+        } else {
+            res.render('sport', { data: rows });
+        }
+    })
+})
+
+//display add sport page
+router.get('/add',(req, res, next) => {
+    res.render('sport/add',{
+        sportName:'',
+        sportPlaynum:'',
+        type:''
+    })
+})
+
+// add new sport
+router.post('/add', (req, res, next) =>{
+    let sportName = req.body.sportName;
+    let sportPlaynum = req.body.sportPlaynum;
+    let type = req.body.type;
+    let errors = false;
+
+    if(sportName.length === 0) {
+        errors = true;
+        //set flash message
+        req.flash('error', 'โปรดกรอก');
+        //render to add.ejs with flash message
+        res.render('sport/add', {
+            sportName: sportName,
+            sportPlaynum: sportPlaynum,
+            type:type
+        })
+    }
+
+    // if no error
+    if(!errors) {
+        let form_data = {
+            sportName: sportName,
+            sportPlaynum: sportPlaynum,
+            type:type
+        }
+        // insert query db
+        dbConnection.query('INSERT INTO sport SET ?', form_data, (err, result) => {
+            if (err) {
+                req.flash('error', err)
+
+                res.render('sport/add', {
+                    sportName: form_data.sportName,
+                    sportPlaynum: form_data.sportPlaynum,
+                    type: form_data.type
+                })
+            } else {
+                req.flash('success', 'sport successfully added');
+                res.redirect('/sport');
+            }
+        })
+    }
+})
+
+// display edit book page
+router.get('/edit/(:sportID)', (req, res, next) => {
+    let sportID = req.params.sportID;
+
+    dbConnection.query('SELECT * FROM sport WHERE sportID = ' + sportID, (err, rows, fields) => {
+        if (rows.length <= 0) {
+            req.flash('error', 'Book not found with id = ' + sportID)
+            res.redirect('/sport');
+        } else {
+            res.render('sport/edit', {
+                title: 'แก้ไข กีฬา',
+                sportID: rows[0].sportID,
+                sportName: rows[0].sportName,
+                sportPlaynum: rows[0].sportPlaynum,
+                type: rows[0].type
+            })
+        }
+    });
+})
+
+// update book page
+router.post('/update/:sportID', (req, res, next) => {
+    let sportID = req.params.sportID;
+    let sportName = req.body.sportName;
+    let sportPlaynum = req.body.sportPlaynum;
+    let type = req.body.type;
+    let errors = false;
+
+    if (sportName.length === 0) {
+        errors = true;
+        req.flash('error', 'Please enter name and author');
+        res.render('sport/edit', {
+            sportID: req.params.sportID,
+            sportName: sportName,
+            sportPlaynum: sportPlaynum,
+            typeI: type
+        })
+    }
+    // if no error
+    if (!errors) {
+        let form_data = {
+            sportName:  sportName,
+            sportPlaynum: sportPlaynum,
+            type: type
+        }
+        // update query
+        dbConnection.query("UPDATE sport SET ? WHERE sportID = " + sportID, form_data, (err, result) => {
+            if (err) {
+                req.flash('error', err);
+                res.render('sport/edit', {
+                    sportID: req.params.sportID,
+                    sportName: form_data.sportName,
+                    sportPlaynum: form_data.sportPlaynum,
+                    type: form_data.type
+                })
+            } else {
+                req.flash('success', 'sport successfully updated');
+                res.redirect('/sport')
+            }
+        })
+    }
+})
+
+// delete sport
+router.get('/delete/(:sportID)', (req, res, next) => {
+    let sportID = req.params.sportID;
+
+    dbConnection.query('DELETE FROM sport WHERE sportID = ' + sportID, (err, result) => {
+        if (err) {
+            req.flash('error', err),
+            res.redirect('/sport');
+        } else {
+            req.flash('success', 'sport successfully deleted! ID = ' + sportID);
+            res.redirect('/sport');
+        }
+    })
+})
+
+module.exports = router;
