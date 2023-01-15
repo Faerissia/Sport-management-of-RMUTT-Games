4af7f52a977e643be14852162892837e9c2ccb5c
FAERIS <firstlnw0099@gmail.com>
2023-01-15 22:10:57 +0700

    update 15/01/2566 22.10

--- routes/tournament.js
+++ routes/tournament.js
@@ -226,16 +226,16 @@ router.get('/bracket/(:tnmID)', (req, res, next)=> {
 //หน้าผู้เข้าร่วม
 router.get('/participant/(:tnmID)', (req, res, next)=> {
     let tnmID = req.params.tnmID;
-    dbConnection.query('SELECT p.playerID,p.playerFName,p.playerLName,p.playerGender,TIMESTAMPDIFF(YEAR, p.playerBirthday, CURDATE()) AS age,p.playerPhone,p.playerRegDate,p.playerStatus,p.teamID,t.tnmID,t.tnmName FROM player p LEFT JOIN tournament t on p.tnmID = t.tnmID WHERE t.tnmID = '+tnmID, (err, rows) => {
+    dbConnection.query('SELECT p.playerID,p.playerFName,p.playerLName,p.playerGender,TIMESTAMPDIFF(YEAR, p.playerBirthday, CURDATE()) AS age,p.playerPhone,p.playerRegDate,p.playerStatus,p.teamID,t.tnmID,t.tnmName,s.type FROM player p LEFT JOIN tournament t on p.tnmID = t.tnmID LEFT JOIN sport s ON t.sportID = s.sportID WHERE t.tnmID = '+tnmID, (err, rows) => {
         if(req.session.loggedin){
         if(role === 'เจ้าหน้าที่'){
             if(rows[0].teamID === null){
             res.render('tournament/participant', { data: rows,tnmID:tnmID,status_login: req.session.loggedin,user: user});
-        }else{
-            dbConnection.query('SELECT team.*,t.* FROM team team LEFT JOIN tournament t ON team.tnmID = t.tnmID WHERE t.tnmID = '+tnmID,(err, rows) => {
-                res.render('tournament/participant', { data: rows,tnmID:tnmID,status_login: req.session.loggedin,user: user});
-            })
-        }
+            }else{
+                dbConnection.query('SELECT * FROM team Where tnmID = '+tnmID, (err, rows) => {
+                    res.render('tournament/participant', { data: rows,tnmID:tnmID,status_login: req.session.loggedin,user: user});
+                })
+            }
         }else{
             req.flash('error','ไม่สามารถเข้าถึงได้');
             res.redirect('login');
